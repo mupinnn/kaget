2f91e66afe1003e4d4d106219c3916d6fcb2aad3
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-09-13 22:03:16 +0700

    wip: base boilerplate

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Options} */
+export default {
+  printWidth: 100,
+  arrowParens: "avoid",
+  tabWidth: 2,
+  endOfLine: "lf",
+  quoteProps: "preserve",
+  trailingComma: "es5",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <h1>The quick brown fox jumps overt the lazy dog.</h1>
+  </StrictMode>
+);
