9d478ee18fcf5becc15994e8d88b6d4f67061254
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-11-09 23:26:33 +0700

    feat: wallet list

--- src/features/wallets/components/wallet-card.tsx
+++ src/features/wallets/components/wallet-card.tsx
@@ -1,18 +1,34 @@
 import * as React from "react";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import { formatCurrency } from "@/utils/common.util";
 
 export interface WalletCardProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string;
+  digitalBalance: number;
+  cashBalance: number;
   totalBalance: number;
 }
 
 export const WalletCard = React.forwardRef<HTMLDivElement, WalletCardProps>(
-  ({ name, totalBalance, ...props }, ref) => (
+  ({ name, totalBalance, cashBalance, digitalBalance, ...props }, ref) => (
     <Card ref={ref} {...props}>
       <CardHeader>
         <CardTitle>{name}</CardTitle>
-        <CardDescription>{formatCurrency(totalBalance)}</CardDescription>
+        <CardDescription className="space-y-1">
+          <span>{formatCurrency(totalBalance)}</span>
+          <Progress value={80} />
+          <div className="space-y-1 text-xs">
+            <div className="flex items-center gap-1">
+              <div className="h-2 w-2 bg-primary" />
+              <span>Digital - {formatCurrency(digitalBalance)} (80%)</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="h-2 w-2 bg-primary/20" />
+              <span>Cash - {formatCurrency(cashBalance)} (20%)</span>
+            </div>
+          </div>
+        </CardDescription>
       </CardHeader>
     </Card>
   )
--- src/features/wallets/pages/wallets.page.tsx
+++ src/features/wallets/pages/wallets.page.tsx
@@ -1,14 +1,33 @@
-import { useWallets } from "../data/wallets.queries";
+import { Link } from "@tanstack/react-router";
+import { WalletCard } from "../components/wallet-card";
+
+const wallets = Array.from({ length: 10 }).map((_, i) => ({
+  id: i,
+  name: `Wallet ${i}`,
+  cash_balance: 1000,
+  digital_balance: 95001000,
+  total_balance: 95002000,
+}));
 
 export function WalletsIndexPage() {
-  const walletsQuery = useWallets();
-
-  if (walletsQuery.isPending) return <p>Loading . . .</p>;
-  if (walletsQuery.isError) return <p>An error ocurred.</p>;
-
   return (
-    <p>
-      <pre>{JSON.stringify(walletsQuery.data, null, 2)}</pre>
-    </p>
+    <div className="space-y-4">
+      <div className="space-y-1">
+        <h1 className="text-3xl font-bold">Wallets</h1>
+        <p className="text-sm text-muted-foreground">Manage your wallets. Hassle-free.</p>
+      </div>
+      <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-4">
+        {wallets.map(wallet => (
+          <Link to="/wallets" key={wallet.id} className="no-underline">
+            <WalletCard
+              name={wallet.name}
+              totalBalance={wallet.total_balance}
+              cashBalance={wallet.cash_balance}
+              digitalBalance={wallet.digital_balance}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 }
