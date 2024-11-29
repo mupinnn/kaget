57e5737791c8cfc29536d98bc98a724f153ee8da
Ahmad Muwaffaq <itsmupin@gmail.com>
2024-11-29 17:43:45 +0700

    feat: reusable record list

--- src/features/records/pages/records.page.tsx
+++ src/features/records/pages/records.page.tsx
@@ -1,17 +1,10 @@
 import { Link } from "@tanstack/react-router";
-import { PlusIcon, ArrowRightLeftIcon } from "lucide-react";
-import { P, match } from "ts-pattern";
+import { PlusIcon } from "lucide-react";
+import { match } from "ts-pattern";
 import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import { EmptyState } from "@/components/empty-state";
 import { PageLayout } from "@/components/page-layout";
-import { formatCurrency, formatDate } from "@/utils/common.util";
-import { cn } from "@/libs/utils.lib";
 import { useRecordsQuery } from "../data/records.queries";
-
-const loaders = Array.from({ length: 5 }).map((_, i) => (
-  <Skeleton key={i} className="h-24 w-full" />
-));
+import { RecordList, RecordListLoader } from "../components/record-list";
 
 export function RecordsIndexPage() {
   const recordsQuery = useRecordsQuery();
@@ -28,45 +21,12 @@ export function RecordsIndexPage() {
         </Link>
       </Button>
 
-      <div className="divide-y">
-        {match(recordsQuery)
-          .with({ isPending: true }, () => loaders)
-          .with({ isError: true }, () => <p>An error occured</p>)
-          .with({ data: P.select(P.when(data => data.data.length === 0)) }, () => (
-            <EmptyState
-              title="No record created"
-              description="You have not record any transaction. Record one above."
-              icon={ArrowRightLeftIcon}
-            />
-          ))
-          .otherwise(() =>
-            recordsQuery.data?.data.map(record => {
-              const isIncome = record.record_type === "INCOME";
-              return (
-                <Link
-                  to="/records/$recordId"
-                  params={{ recordId: record.id }}
-                  className="flex items-center justify-between py-2 text-sm no-underline"
-                  key={record.id}
-                >
-                  <div className="space-y-1">
-                    <p>{record.note}</p>
-                    <p className="text-xs">
-                      {record.source.name} - {record.source_type}
-                    </p>
-                  </div>
-                  <div className="space-y-1 text-right">
-                    <p className={cn(isIncome ? "text-success" : "text-destructive")}>
-                      {isIncome ? "+" : "-"}
-                      {formatCurrency(record.amount)}
-                    </p>
-                    <p className="text-xs">{formatDate(record.recorded_at)}</p>
-                  </div>
-                </Link>
-              );
-            })
-          )}
-      </div>
+      {match(recordsQuery)
+        .with({ isPending: true }, () => <RecordListLoader />)
+        .with({ isError: true }, () => <p>An error occured</p>)
+        .otherwise(() => (
+          <RecordList data={recordsQuery.data?.data} />
+        ))}
     </PageLayout>
   );
 }
