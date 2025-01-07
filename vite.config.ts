6980685b4d05d38c245d3e8d94642ccb15f7d386
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-01-07 15:44:43 +0700

    chore: pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,44 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: ["**/*"],
+      manifest: {
+        name: "KaGet: Kawan Budget",
+        short_name: "KaGet",
+        description: "A fully offline web-based budgeting app that meets your needs",
+        theme_color: "#facc15",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,icon,png,svg,woff,woff2,ttf,otf}"],
+      },
+    }),
+  ],
 
   resolve: {
     alias: {
