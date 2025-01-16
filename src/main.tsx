822925c347aed319cd4a66e5913cbe3a95eaed0b
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-01-16 20:32:00 +0700

    chore: basic sentry setup (#15)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import { App } from "./app";
 import { preloadSettings } from "./features/settings/data/settings.services";
 
 import "@fontsource-variable/inter";
 import "@fontsource-variable/inter/wght-italic.css";
 import "./index.css";
+
+Sentry.init({
+  dsn: "https://df7ed22435a9e42616c5c2a58ada6307@o4508653484769280.ingest.de.sentry.io/4508653487259728",
+  integrations: [],
+});
 
 async function boot() {
   await preloadSettings();
