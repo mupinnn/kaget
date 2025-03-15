2425289dd30be68029a0028564fc8a3454d7affa
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-03-15 12:19:59 +0700

    test(e2e): dashboard

--- e2e/integration.spec.ts
+++ e2e/integration.spec.ts
@@ -4,12 +4,12 @@ test.use({
   locale: "en-US",
 });
 
-test.beforeAll("Setup", async ({ page }) => {
+test.beforeEach("Setup", async ({ page }) => {
   await page.clock.setFixedTime(new Date("2024-02-02T10:00:00"));
   await page.goto("/");
 });
 
-test("KaGet app should running just fine", async ({ page, baseURL }) => {
+test("KaGet app should running just fine", async ({ page }) => {
   await test.step("Onboarding", async () => {
     await test.step("Should land on onboarding page first", async () => {
       await expect(page).toHaveURL(/onboarding/);
@@ -49,9 +49,56 @@ test("KaGet app should running just fine", async ({ page, baseURL }) => {
     });
   });
 
-  test.fixme("Should be able to go to create wallet from dashboard", async () => {
-    console.log(new Date().toLocaleDateString());
-    expect(page.url()).toBe(baseURL);
-    await expect(page.getByRole("link", { name: "Create a wallet" })).toBeVisible();
+  await test.step("Dashboard", async () => {
+    await test.step("Should have empty wallets, budgets, and today's recap widgets", async () => {
+      await expect(page.getByRole("heading", { name: "No wallet created" })).toBeVisible();
+      await expect(page.getByRole("heading", { name: "No budget created" })).toBeVisible();
+      await expect(page.getByRole("heading", { name: "No records created" })).toBeVisible();
+    });
+
+    await test.step("Should have functioning sidebar", async () => {
+      await expect(page.locator("[data-variant=sidebar][data-state=expanded]")).toBeVisible();
+
+      await page.getByRole("button", { name: "Toggle sidebar" }).click();
+      await expect(page.locator("[data-variant=sidebar][data-state=collapsed]")).toBeVisible();
+
+      await page.getByRole("button", { name: "Toggle sidebar" }).click();
+      await expect(page.locator("[data-variant=sidebar][data-state=expanded]")).toBeVisible();
+
+      await test.step("Go to Wallets page", async () => {
+        await page.getByRole("link", { name: "Wallets", exact: true }).click();
+        await page.waitForURL("/wallets");
+        await expect(page).toHaveURL("/wallets");
+        await expect(page.getByRole("heading", { name: "Wallets", level: 1 })).toBeVisible();
+      });
+
+      await test.step("Go to Records page", async () => {
+        await page.getByRole("link", { name: "Records", exact: true }).click();
+        await page.waitForURL("/records");
+        await expect(page).toHaveURL("/records");
+        await expect(page.getByRole("heading", { name: "Records", level: 1 })).toBeVisible();
+      });
+
+      await test.step("Go to Budgets page", async () => {
+        await page.getByRole("link", { name: "Budgets", exact: true }).click();
+        await page.waitForURL("/budgets");
+        await expect(page).toHaveURL("/budgets");
+        await expect(page.getByRole("heading", { name: "Budgets", level: 1 })).toBeVisible();
+      });
+
+      await test.step("Go to Transfers page", async () => {
+        await page.getByRole("link", { name: "Transfers", exact: true }).click();
+        await page.waitForURL("/transfers");
+        await expect(page).toHaveURL("/transfers");
+        await expect(page.getByRole("heading", { name: "Transfers", level: 1 })).toBeVisible();
+      });
+
+      await test.step("Go to Settings page", async () => {
+        await page.getByRole("link", { name: "Settings", exact: true }).click();
+        await page.waitForURL("/settings");
+        await expect(page).toHaveURL("/settings");
+        await expect(page.getByRole("heading", { name: "Settings", level: 1 })).toBeVisible();
+      });
+    });
   });
 });
