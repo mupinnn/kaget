f1c9a503eb6f314644161c9166fe0c6368aeab33
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-03-15 14:24:33 +0700

    test(e2e): wallets

--- e2e/integration.spec.ts
+++ e2e/integration.spec.ts
@@ -9,7 +9,7 @@ test.beforeEach("Setup", async ({ page }) => {
   await page.goto("/");
 });
 
-test("KaGet app should running just fine", async ({ page }) => {
+test("KaGet app should running just fine", async ({ page, baseURL }) => {
   await test.step("Onboarding", async () => {
     await test.step("Should land on onboarding page first", async () => {
       await expect(page).toHaveURL(/onboarding/);
@@ -49,7 +49,7 @@ test("KaGet app should running just fine", async ({ page }) => {
     });
   });
 
-  await test.step("Dashboard", async () => {
+  await test.step("Home", async () => {
     await test.step("Should have empty wallets, budgets, and today's recap widgets", async () => {
       await expect(page.getByRole("heading", { name: "No wallet created" })).toBeVisible();
       await expect(page.getByRole("heading", { name: "No budget created" })).toBeVisible();
@@ -98,6 +98,101 @@ test("KaGet app should running just fine", async ({ page }) => {
         await page.waitForURL("/settings");
         await expect(page).toHaveURL("/settings");
         await expect(page.getByRole("heading", { name: "Settings", level: 1 })).toBeVisible();
+      });
+
+      await test.step("Go to Home page", async () => {
+        await page.getByRole("link", { name: "Home", exact: true }).click();
+        await page.waitForURL("/");
+        expect(page.url()).toBe(baseURL);
+      });
+    });
+  });
+
+  await test.step("Wallets", async () => {
+    await test.step("Create a wallet", async () => {
+      await page.getByRole("link", { name: "Create a wallet" }).click();
+      await page.waitForURL("/wallets/create");
+      await expect(page.getByRole("heading", { name: "Create wallet", level: 1 })).toBeVisible();
+
+      await page.getByLabel("Name").fill("BXA");
+      await page.getByLabel("Initial balance").fill("100000");
+      await page.getByText("Choose a wallet type").click();
+      await page.getByRole("option", { name: "DIGITAL" }).click();
+      await expect(page.getByRole("combobox", { name: "Type" })).toHaveText("DIGITAL");
+
+      await page.getByRole("button", { name: "Save" }).click();
+      await page.waitForURL("/wallets");
+
+      await page.getByRole("link", { name: "Create new wallet" }).click();
+      await page.waitForURL("/wallets/create");
+      await expect(page.getByRole("heading", { name: "Create wallet", level: 1 })).toBeVisible();
+
+      await page.getByLabel("Name").fill("JAGX");
+      await page.getByLabel("Initial balance").fill("50000");
+      await page.getByText("Choose a wallet type").click();
+      await page.getByRole("option", { name: "CASH" }).click();
+      await expect(page.getByRole("combobox", { name: "Type" })).toHaveText("CASH");
+
+      await page.getByRole("button", { name: "Save" }).click();
+      await page.waitForURL("/wallets");
+
+      await page.getByRole("link", { name: "Create new wallet" }).click();
+      await page.waitForURL("/wallets/create");
+      await expect(page.getByRole("heading", { name: "Create wallet", level: 1 })).toBeVisible();
+
+      await page.getByLabel("Name").fill("BNX");
+      await page.getByLabel("Initial balance").fill("25000");
+      await page.getByText("Choose a wallet type").click();
+      await page.getByRole("option", { name: "DIGITAL" }).click();
+      await expect(page.getByRole("combobox", { name: "Type" })).toHaveText("DIGITAL");
+
+      await page.getByRole("button", { name: "Save" }).click();
+      await page.waitForURL("/wallets");
+
+      await expect(page.getByRole("heading", { name: "No wallet created" })).toBeHidden();
+      await expect(page.locator('[data-testid=wallet-list] > a[href^="/wallets/"]')).toHaveCount(2);
+    });
+
+    await test.step("Wallet detail", async () => {
+      await page.getByText("BXA").click();
+      await page.waitForURL("/wallets/**");
+
+      await expect(page.getByRole("heading", { name: "BXA" })).toBeVisible();
+      await expect(page.getByRole("link", { name: "Edit" })).toBeVisible();
+      await expect(page.getByRole("button", { name: "Delete" })).toBeVisible();
+      await expect(page.getByRole("heading", { name: "No records created" })).toBeVisible();
+
+      await page.getByRole("tab", { name: "Transfers" }).click();
+      await expect(page.getByRole("heading", { name: "No transfer yet" })).toBeVisible();
+
+      await test.step("Edit wallet name", async () => {
+        await page.getByRole("link", { name: "Edit" }).click();
+        await page.waitForURL("/wallets/**/edit");
+
+        await expect(page.getByRole("heading", { name: "Update BXA wallet" })).toBeVisible();
+
+        await page.getByLabel("Name").fill("BXI");
+        await page.getByRole("button", { name: "Save" }).click();
+        await page.waitForURL("/wallets/**");
+
+        await expect(page.getByRole("heading", { name: "BXI" })).toBeVisible();
+      });
+
+      await test.step("Delete a wallet", async () => {
+        await page.goto("/wallets");
+        await page.waitForURL("/wallets");
+
+        await page.getByText("JAGX").click();
+        await page.waitForURL("/wallets/**");
+
+        await page.getByRole("button", { name: "Delete" }).click();
+        await page.getByRole("button", { name: "Yes, delete" }).click();
+        await page.waitForURL("/wallets");
+
+        await expect(page.getByText("JAGX")).toBeHidden();
+        await expect(page.locator('[data-testid=wallet-list] > a[href^="/wallets/"]')).toHaveCount(
+          1
+        );
       });
     });
   });
